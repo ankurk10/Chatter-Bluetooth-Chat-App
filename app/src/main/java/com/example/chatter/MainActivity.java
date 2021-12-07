a1710ca7d843e9b8e1ca83de540068e59a774d61
ankurk10 <59817633+ankurk10@users.noreply.github.com>
2021-12-08 03:20:04 +0530

    Added Bluetooth Request Functionality

--- app/src/main/java/com/example/chatter/MainActivity.java
+++ app/src/main/java/com/example/chatter/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.chatter;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private final int LOCATION_PERMISSION_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +27,89 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private void initbluetooth()
+    {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null)
+        {
+            Toast.makeText(this, "No Bluetooth Found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_search_devices:
+                checkPermissions();
+                Toast.makeText(this, "Clicked Search Devices", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_enable_bluetooth:
+                enableBluetooth();
+                Toast.makeText(this, "Clicked Enable Bluetooth", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void enableBluetooth()
+    {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter.isEnabled())
+        {
+            Toast.makeText(this, "Bluetooth already enabled", Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+            bluetoothAdapter.enable();
+
+        }
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
+        } else {
+            Intent intent = new Intent(this, DeviceListActivity.class);
+            startActivity(intent);
+            //startActivityForResult(intent, SELECT_DEVICE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(this, DeviceListActivity.class);
+                startActivity(intent);
+                //startActivityForResult(intent, SELECT_DEVICE);
+            } else {
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setMessage("Location permission is required.\n Please grant")
+                        .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                checkPermissions();
+                            }
+                        })
+                        .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                MainActivity.this.finish();
+                            }
+                        }).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
